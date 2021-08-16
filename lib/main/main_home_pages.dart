c98766897376cb9b8518cf234c3b90c720ae1bc3
taowang <twang@telenav.cn>
2021-08-16 09:30:17 +0800

    add search button

--- lib/main/main_home_pages.dart
+++ lib/main/main_home_pages.dart
@@ -9,31 +9,67 @@ class MainHomePage extends StatefulWidget   {
 }
 
 class MainHomePageState extends State<MainHomePage> with SingleTickerProviderStateMixin{
-  List<String> topTabList = ["推荐", "动态", "Java", "Flutter"];
+  List<String> topTabList = ["推荐", "动态", "Java", "Flutter", "K8S", "Helm", "Chart", "Git"];
 
-
+  TabController? tabController;
 
   @override
   void initState() {
     super.initState();
+    tabController = new TabController(length: 8, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            bottom: TabBar(
-              controller: new TabController(length: topTabList.length, vsync: this),
-              tabs: topTabList
-                  .map((e) => Tab(
-                        text: e,
-                      ))
-                  .toList(),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext buildContext,bool flag){
+        return [SliverAppBar(
+          pinned: true,
+          floating: true,
+          actions: [
+            IconButton( icon: Icon(Icons.add),onPressed: (){
+              print("点击了添加");
+            },)
+          ],
+          title: Ink(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.all(Radius.circular(6))
             ),
-          )
-        ],
+            child: InkWell(
+              onTap: (){
+                print("点击了搜索框");
+              },
+              child: Container(
+                height: 33,
+                width: 400,
+                child: Row(
+                  children: [
+                    SizedBox(width: 12,),
+                    Icon(Icons.search,size: 18,),
+                    SizedBox(width: 12,),
+                    Text("搜索",style: TextStyle(fontSize: 14),)
+                  ],
+                ),
+              ),
+            ),
+          ),
+          bottom: TabBar(
+            isScrollable: true,
+            controller: tabController,
+            tabs: topTabList
+                .map((e) => Tab(
+              text: e,
+            ))
+                .toList(),
+          ),
+        )];
+        },
+        body: TabBarView(
+          controller: tabController,
+          children: topTabList.map((e) => Center(child: Text("$e"),)).toList(),
+        ),
       ),
     );
   }
